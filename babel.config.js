67397699b98f549c69d5eecc21221b4aa3109241
melquisedeque_sodre <43079783+melquisedeque-magalhaes@users.noreply.github.com>
2022-11-30 23:48:59 -0300

    first commit configurations

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,22 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        "module-resolver", 
+        {
+          "root": ["./src"],
+          "alias": {
+            "@screens": "./src/screens",
+            "@components": "./src/components",
+            "@assets": "./src/assets",
+            "@routes": "./src/routes",
+            "@theme": "./src/theme",
+            "@utils": "./src/utils",
+            "@storage": "./src/storage",
+          }
+        }
+      ]
+    ]
   };
 };
---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+import * as S from "./styles";
+
+export function Groups() {
+  return (
+    <S.Container>
+      <Text>Hello Ignite Teams</Text>
+    </S.Container>
+  )
+}
